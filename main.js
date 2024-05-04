3f61bf292d4f52557a5ec7595ad28b13444ca28a
StivenGonzalezz <grstiven1004@gmail.com>
2024-05-04 01:25:39 -0500

    creacion de proyecto

---
+++ main.js
@@ -0,0 +1,7 @@
+// document.querySelector('.tecla_pom').onclick =()=>{
+//     document.querySelector('#sonido_tecla_pom').play();
+// }
+
+const listaTeclas = document.querySelectorAll('tecla').map
+
+
