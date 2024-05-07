0d73f07c46d67bf4abe449afd2462877f3d95bc3
StivenGonzalezz <grstiven1004@gmail.com>
2024-05-06 20:58:41 -0500

    utlizacion de ciclos for

--- main.js
+++ main.js
@@ -2,6 +2,22 @@
 //     document.querySelector('#sonido_tecla_pom').play();
 // }
 
-const listaTeclas = document.querySelectorAll('tecla').map
+const listaTeclas = document.querySelectorAll('.tecla');
+
+function playSonido(id){
+    document.querySelector(id).play()
+}
+
+
+for(let contador  = 0 ; contador < 9 ; contador++){
+    const tecla = listaTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idSound = `#sonido_${instrumento}`
+    tecla.onclick = function (){
+        playSonido(idSound)
+    }
+    contador++
+    console.log("vuelta " + contador)
+}
 
 
